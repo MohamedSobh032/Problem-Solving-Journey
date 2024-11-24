c6c1bc09136686bab75731d907034ee4013ceec9
Mohamed Sobh <129511963+MohamedSobh032@users.noreply.github.com>
2024-11-24 21:31:53 +0200

    In C++: 461. Hamming Distance, Changed file names to be the same as leetcode's

---
+++ Using C++/461. Hamming Distance.cpp
@@ -0,0 +1,18 @@
+/**
+ * @brief First/Only Solution
+ * ---> O(n), where n is the number of 1's of the xor
+ * ---> Got the algorithm from Computer Networks-1 course
+ */
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        
+        int res = x ^ y;
+        int sum = 0;
+        while (res > 0) {
+            sum += res & 1;
+            res = res >> 1;
+        }
+        return sum;
+    }
+};

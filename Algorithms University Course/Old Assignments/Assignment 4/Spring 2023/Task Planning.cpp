06709e943736f08a999595ed100da7ce67cbba19
Mohamed Sobh <129511963+MohamedSobh032@users.noreply.github.com>
2024-12-12 21:23:42 +0200

    Fix small issue in Task Planning

--- Algorithms University Course/Old Assignments/Assignment 4/Spring 2023/Task Planning.cpp
+++ Algorithms University Course/Old Assignments/Assignment 4/Spring 2023/Task Planning.cpp
@@ -11,25 +11,20 @@ int main() {
     // INPUT PHASE
     int N; std::cin >> N;
     std::vector<std::pair<ll, ll>> tasks(N);
-    for (int i = 0; i < N; i++)
-        std::cin >> tasks[i].second;
-    for (int i = 0; i < N; i++)
-        std::cin >> tasks[i].first;
-    // SORT
-    std::sort(tasks.rbegin(), tasks.rend());
+    for (int i = 0; i < N; i++) std::cin >> tasks[i].second;
+    for (int i = 0; i < N; i++) std::cin >> tasks[i].first;
     
-    std::vector<bool> timetable(N + 1, false);
-    int total_penalty = 0;
-    for (int i = 0; i < (int)tasks.size(); i++) {
-        bool found = false;
-        for (ll j = tasks[i].second; j >= 1; j--) {
-            if (timetable[j] == false) {
-                timetable[j] = true;
-                found = true;
-                break;
-            }
-        }
-        if (!found) total_penalty += tasks[i].first;
+    // SORT VECTOR
+    std::sort(tasks.begin(), tasks.end());
+    std::reverse(tasks.begin(), tasks.end());
+    
+    std::vector<bool> timetable(N, false);
+    ll total_penalty = 0;
+    for (int i = 0; i < N; i++) {
+        int j = tasks[i].second - 1;
+        for (; j >= 0; j--)
+            if (!timetable[j]) { timetable[j] = true; break; }
+        if (j == -1) total_penalty += tasks[i].first;
     }
-    cout << total_penalty;
+    std::cout << total_penalty;
 }

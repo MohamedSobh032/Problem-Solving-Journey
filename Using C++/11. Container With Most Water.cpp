5ec0d2e9cea853ef8707a9e9d4b9fabb429efd05
Mohamed Sobh <129511963+MohamedSobh032@users.noreply.github.com>
2024-12-27 16:26:54 +0200

    In C++: 11. Container With Most Water 2 solutions

---
+++ Using C++/11. Container With Most Water.cpp
@@ -0,0 +1,41 @@
+/**
+ * @brief First Solution
+ * -- O(n^2)
+ * -- Brute force solution
+ */
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        
+        int max_area = 0;
+        // iterate on all possible combinations
+        for (int i = 0; i < height.size(); i++)
+            for (int j = i + 1; j < height.size(); j++)
+                max_area = std::max(max_area, abs(i - j) * std::min(height[i], height[j]));
+
+        return max_area;
+    }
+};
+
+
+/**
+ * @brief Second Solution
+ * -- O(n)
+ * -- Two pointer approach
+ */
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        
+        int l = 0; int r = height.size() - 1;
+        int max_area = 0;
+
+        while (l < r) {
+            max_area = std::max(max_area, (r - l) * std::min(height[r], height[l]));
+            if (height[l] > height[r]) r--;
+            else l++;
+        }
+        
+        return max_area;
+    }
+};

4ddc3a6d70ba64f85bf99750f7798bf9a70953a5
Mohamed Sobh <129511963+MohamedSobh032@users.noreply.github.com>
2024-12-29 21:17:35 +0200

    In Algorithms Course: Update in lab-5 to look cleaner

--- Algorithms University Course/Labs/Lab-5/Watermelon Watermelon.cpp
+++ Algorithms University Course/Labs/Lab-5/Watermelon Watermelon.cpp
@@ -5,37 +5,27 @@ using namespace std;
 
 // bottom up
 int main() {
-
     int n; std::cin >> n;
     int x, y, z; std::cin >> x >> y >> z;
     std::vector<int> dp(n + 1, -1);
     dp[0] = 0;
     for (int i = 1; i <= n; i++) {
-        if (i >= x && dp[i - x] != -1)
-            dp[i] = max(dp[i], dp[i - x] + 1);
-        if (i >= y && dp[i - y] != -1)
-            dp[i] = max(dp[i], dp[i - y] + 1);
-        if (i >= z && dp[i - z] != -1)
-            dp[i] = max(dp[i], dp[i - z] + 1);
+        if (i >= x && dp[i - x] != -1) dp[i] = std::max(dp[i], dp[i - x] + 1);
+        if (i >= y && dp[i - y] != -1) dp[i] = std::max(dp[i], dp[i - y] + 1);
+        if (i >= z && dp[i - z] != -1) dp[i] = std::max(dp[i], dp[i - z] + 1);
     }
-    std::cout << (dp[n] == -1 ? 0 : dp[n]) << endl;
+    std::cout << (dp[n] == -1 ? 0 : dp[n]);
 }
 
 // top down
 int x, y, z;
 int rec(int n, vector<int>& dp) {
 
-    if (n < 0)
-        return -1;
-    if (n == 0)
-        return 0;
-    if (dp[n] != -1)
-        return dp[n];
-
-    int maximum = std::max(INT_MIN, 1 + rec(n - x, dp));
-    maximum = std::max(1 + rec(n - y, dp), 1 + rec(n - z, dp));
-    dp[n] = maximum;
-    return maximum;
+    if (n < 0) return -1;
+    if (n == 0) return 0;
+    if (dp[n] != -1) return dp[n];
+    dp[n] = std::max({1 + rec(n - x, dp), 1 + rec(n - y, dp), 1 + rec(n - z, dp)});
+    return dp[n];
 }
 
 int main() {

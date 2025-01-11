648bfa2596485a281a634a66c484edde8c0b3a6d
Mohamed Sobh <129511963+MohamedSobh032@users.noreply.github.com>
2025-01-11 15:42:19 +0200

    In C++: 1400. Construct K Palindrome Strings -- 3 Solutions

--- Using C++/1. Two Sum.cpp
+++ Using C++/1. Two Sum.cpp
@@ -1,17 +1,18 @@
 /**
  * @brief First Solution
- * -- O(n^2)
- * -- Worst solution
+ * -- Time Complexity: O(n^2)
+ * -- Space Complexity: O(1)
+ * -- Brute-force approach by checking all possible pairs
  */
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        
         for (int i = 0; i < nums.size(); i++) {
             int toGet = target - nums[i];
-            for (int j = 0; j < nums.size(); j++) {
+            for (int j = 0; j < nums.size(); j++)
                 if (toGet == nums[j] && i != j)
                     return {i, j};
-            }
         }
         return {};
     }
@@ -20,22 +21,19 @@ public:
 
 /**
  * @brief Second Solution
- * -- O(n)
- * -- Applies only to sorted arrays
+ * -- Time Complexity: O(n)
+ * -- Space Complexity: O(1)
+ * -- Two-pointer approach, but requires sorted array
  */
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int l = 0;
-        int r = nums.size() - 1;
+        int l = 0, r = nums.size() - 1;
         while (l < r) {
             int total = nums[l] + nums[r];
-            if (total == target)
-                return {l, r};
-            if (total < target)
-                l++;
-            if (total > target)
-                r--;
+            if (total == target) return {l, r};
+            if (total < target) l++;
+            else r--;
         }
         return {};
     }
@@ -43,17 +41,18 @@ public:
 
 /**
  * @brief Third Solution
- * -- O(n)
- * -- Best solution
+ * -- Time Complexity: O(n)
+ * -- Space Complexity: O(n)
+ * -- Using hash map to store the index of the elements
  */
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> nmap;
+        
+        std::unordered_map<int, int> nmap;
         for (int i = 0; i < nums.size(); i++) {
             int toGet = target - nums[i];
-            if (nmap.find(toGet) != nmap.end())
-                return {nmap[toGet], i};
+            if (nmap.find(toGet) != nmap.end()) return {nmap[toGet], i};
             nmap.insert({nums[i], i});
         }
         return {};

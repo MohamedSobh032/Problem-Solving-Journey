88c3ff45f9b809d1116d15ee215cf14755290b8f
Mohamed Sobh <129511963+MohamedSobh032@users.noreply.github.com>
2025-01-28 19:52:29 +0200

    In C++: 206. Reverse Linked List -- Added 1 Solution

--- Using C++/206. Reverse Linked List.cpp
+++ Using C++/206. Reverse Linked List.cpp
@@ -1,23 +1,43 @@
 /**
- * @brief First/Only Solution
- * -- O(n)
- * -- Get the next item, point to the pervious, move to the next item
+ * @brief First Solution
+ * -- Time Complexity: O(n)
+ * -- Space Complexity: O(1)
+ * -- Iterative two-pointers approach
  */
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-
-        // condition check
-        if (head == nullptr || head->next == nullptr)
-            return head;
-        ListNode* prev = nullptr;
-        ListNode* curr = head;
-        while (curr) {
-            ListNode* temp = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = temp;
+    
+        if (!head) return nullptr;
+        ListNode* tail = nullptr;
+        ListNode* lead = head;
+        while (lead) {
+            ListNode* temp = lead->next;
+            lead->next = tail;
+            tail = lead;
+            lead = temp;
         }
-        return prev;
+        return tail;
+    }
+};
+
+/**
+ * @brief Second Solution
+ * -- Time Complexity: O(n)
+ * -- Space Complexity: O(1)
+ * -- Recursive approach
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+    
+        if (!head) return nullptr;
+        ListNode* cur = head;
+        if (head->next) {
+            cur = reverseList(head->next);
+            head->next->next = head;
+        }
+        head->next = nullptr;
+        return cur;
     }
 };

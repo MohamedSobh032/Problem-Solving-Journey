93fc4d54a3f8790d442ff5a99544c4115a6da83d
Mohamed Sobh <129511963+MohamedSobh032@users.noreply.github.com>
2025-02-26 22:15:57 +0200

    In C++: 88. Merge Sorted Array -- 1 Solution

---
+++ Using C++/88. Merge Sorted Array.cpp
@@ -0,0 +1,17 @@
+/**
+ * @brief First Solution
+ * -- Time Complexity: O(n + m)
+ * -- Space Complexity: O(1)
+ * -- Uses 3 pointers to point to the last element of the 3 arrays
+ */
+class Solution
+{
+public:
+    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+    {
+
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (j >= 0)
+            nums1[k--] = (i >= 0 && nums1[i] > nums2[j]) ? nums1[i--] : nums2[j--];
+    }
+};

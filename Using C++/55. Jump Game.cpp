515383ea6f5c9819f2cb93d8db75ac98d977a27e
Mohamed Sobh <129511963+MohamedSobh032@users.noreply.github.com>
2025-03-14 04:47:42 +0200

    In C++: revisited 55. Jump Game for the 2 Solutions

--- Using C++/55. Jump Game.cpp
+++ Using C++/55. Jump Game.cpp
@@ -1,22 +1,22 @@
 /**
  * @brief First Solution
- * ---> O(n^2)
- * ---> Bottom up dynamic programming approach
+ * -- Time Complexity: O(n^2)
+ * -- Space Complexity: O(n)
+ * -- Brute-force approach, for every element, check if it can jump to any true element
  */
 class Solution
 {
 public:
     bool canJump(vector<int> &nums)
     {
-        std::vector<bool> dp(nums.size(), false);
-        dp[nums.size() - 1] = true;
-        for (int i = nums.size() - 2; i >= 0; i--)
+        int n = nums.size();
+        std::vector<bool> dp(n, false);
+        dp[n - 1] = true;
+        for (int i = n - 2; i >= 0; i--)
         {
-            for (int j = i + 1; j < i + 1 + nums[i]; j++)
+            for (int j = 1; j <= nums[i]; j++)
             {
-                if (j >= nums.size())
-                    break;
-                if (dp[j])
+                if (dp[i + j])
                 {
                     dp[i] = true;
                     break;
@@ -29,8 +29,9 @@ public:
 
 /**
  * @brief Second Solution
- * ---> O(n)
- * ---> Bottom up dynamic programming approach
+ * -- Time Complexity: O(n)
+ * -- Space Complexity: O(1)
+ * -- Bottom-up dynamic programming approach
  */
 class Solution
 {

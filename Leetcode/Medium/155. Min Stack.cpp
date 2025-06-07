3541bad23355cf354cb8df0701e4f4d7141f4a7d
Mohamed Sobh <129511963+MohamedSobh032@users.noreply.github.com>
2025-06-07 20:45:41 +0300

    Leetcode: C++: 155. Min Stack: 1 Solution

---
+++ Leetcode/Medium/155. Min Stack.cpp
@@ -0,0 +1,36 @@
+#include <stack>
+using namespace std;
+
+class MinStack
+{
+private:
+    stack<int> s;
+    stack<int> min_s;
+
+public:
+    MinStack() {}
+
+    void push(int val)
+    {
+        s.push(val);
+        if (min_s.empty() || min_s.top() >= val)
+            min_s.push(val);
+    }
+
+    void pop()
+    {
+        if (s.top() == min_s.top())
+            min_s.pop();
+        s.pop();
+    }
+
+    int top()
+    {
+        return s.top();
+    }
+
+    int getMin()
+    {
+        return min_s.top();
+    }
+};

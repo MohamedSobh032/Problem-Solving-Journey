7736c9e8cfd977d40f5f5ac21ab4dd39763d0391
Mohamed Sobh <129511963+MohamedSobh032@users.noreply.github.com>
2025-06-11 17:41:55 +0300

    Leetcode: C++: 198. House Robber: 1 Solution

---
+++ Leetcode/Medium/198. House Robber.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int rob(vector<int> &nums)
+    {
+        int n = nums.size();
+        if (n == 1)
+            return nums[0];
+
+        int n1 = nums[0], n2 = max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++)
+        {
+            int temp = n2;
+            n2 = max(nums[i] + n1, n2);
+            n1 = temp;
+        }
+        return n2;
+    }
+};
